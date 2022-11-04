3a11516828c05962f17be6edea19702f39aca75c
Dustin Campbell <dustin@teamcampbell.org>
2022-11-04 14:48:57 -0700

    Remove Person constructors

--- src/DotNetConf2022/Person.cs
+++ src/DotNetConf2022/Person.cs
@@ -3,12 +3,4 @@
     public string FirstName { get; init; }
     public string? MiddleName { get; init; }
     public string LastName { get; init; }
-    public Person(string firstName, string? middleName, string lastName)
-    {
-        (FirstName, MiddleName, LastName) = (firstName, middleName, lastName);
-    }
-    public Person(string firstName, string lastName)
-    {
-        (FirstName, LastName) = (firstName, lastName);
-    }
 }
